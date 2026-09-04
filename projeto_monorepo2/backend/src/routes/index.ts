c74fc8b914f86094f8c74e77ece6c667ee6668dc
ricardosugano <sugano.ricardo@gmail.com>
2026-09-03 21:03:28 -0300

    fim

--- projeto_monorepo2/backend/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { userRoutes } from "./userRoutes";
-
-const router = Router();
-
-// Registra as rotas de usuários sob o prefixo /users
-router.use('/users', userRoutes);
-
-export { router as appRoutes };
